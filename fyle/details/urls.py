f3218aa673a7fca56beb30ce9a8af8e0f5710570
ankit.mishra <aknkit.mishra@monoxor.com>
2019-06-20 17:00:20 +0530

    changing the url variable type

--- fyle/details/urls.py
+++ fyle/details/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('api/banks/', views.ApiList.as_view()),
-    path('api/banks/<int:ifsc>/', views.ApiDetail.as_view()),
+    path('api/banks/<slug:ifsc>/', views.ApiDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
