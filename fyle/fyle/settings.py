f6dc9bbdff2ae3d7a640aad4f10a00b361d916ff
ankit.mishra <aknkit.mishra@monoxor.com>
2019-06-20 18:13:51 +0530

    fixing the auth

--- fyle/fyle/settings.py
+++ fyle/fyle/settings.py
@@ -33,9 +33,14 @@ ALLOWED_HOSTS = ["*","ankit-fyle.herokuapp.com"]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 INSTALLED_APPS = [
@@ -46,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
+    'rest_framework_jwt',
     'details',
 ]
 
--- fyle/fyle/urls.py
+++ fyle/fyle/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/',obtain_jwt_token, name='token_obtain_pair'),
+    path('api/token/refresh/', refresh_jwt_token, name='token_refresh'),
     path('', include('details.urls')),
 ]
